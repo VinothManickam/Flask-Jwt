38cb24c994acf9544898fed5f629f565d2cd6f5f
vinoth <vino@gmail.com>
2023-06-22 22:35:21 +0530

    new api

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import datetime
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins='https://stackblitz-starters-aphhzj.stackblitz.io')
 
 app.config['SECRET_KEY'] = 'Thisissecretkey'
 
