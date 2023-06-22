2159bd506b37cf60cdf2d6255d9394b30e5db21b
vinoth <vino@gmail.com>
2023-06-22 20:37:39 +0530

    New api

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, jsonify, request, session, make_response, render_templa
 from functools import wraps
 import jwt
 import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SECRET_KEY'] = 'Thisissecretkey'
 
