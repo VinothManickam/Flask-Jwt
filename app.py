c5e15089918dbe6d69a4cdcc058588f54bde72c1
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2023-06-30 11:49:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ CORS(app)
 
 app.config['SECRET_KEY'] = 'Thisissecretkey'
 
+
 def check_for_token(func):
     @wraps(func)
     def wrapped(*args, **kwargs):
@@ -18,7 +19,8 @@ def check_for_token(func):
         try:
             print(app.config['SECRET_KEY'])
             print(token)
-            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            data = jwt.decode(
+                token, app.config['SECRET_KEY'], algorithms=['HS256'])
             print(data)
         except jwt.InvalidTokenError as e:
             print("Invalid token:", e)
@@ -26,33 +28,43 @@ def check_for_token(func):
         return func(*args, **kwargs)
     return wrapped
 
+
 @app.route('/')
 def index():
-    if not session.get('logged_in'):
-        return render_template('login.html')
-    else:
-        return 'Currently logged in'
+    data = {
+        'message': 'new'
+    }
+    return jsonify(data)
 
 @app.route('/public')
 def public():
-    return 'Anyone can view this'
+    return jsonify(message='Anyone can view this')
 
 @app.route('/auth')
 @check_for_token
 def authorised():
-    return 'This is only viewable with a token'
-@app.route('/login', methods=['POST'])
+    return jsonify(message='This is only viewable with a token')
+
+
 @app.route('/login', methods=['POST'])
 def login():
-    if request.form['username'] and request.form['password'] == 'password':
+    if not request.is_json:
+        return jsonify({"error": "Invalid JSON data"}), 400
+
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if username and password == 'password':
         session['logged_in'] = True
         token = jwt.encode({
-            'user': request.form['username'],
+            'user': username,
             'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60)
         }, app.config['SECRET_KEY'], algorithm='HS256')
         return jsonify({'token': token})
     else:
         return make_response('Unable to verify', 403, {'www-Authenticate': 'Basic realm="login Required"'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
